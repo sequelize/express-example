d73a963c5b3cd187b4cdb2beae8006d03eaea96b
Sascha Depold <sascha@depold.com>
2013-12-09 18:20:01 +0100

    removed reference to global as modules are cached

--- models/index.js
+++ models/index.js
@@ -1,26 +1,23 @@
-if (!global.hasOwnProperty('db')) {
-  var fs        = require('fs')
-    , path      = require('path')
-    , Sequelize = require('sequelize')
-    , lodash    = require('lodash')
-    , sequelize = new Sequelize('sequelize_test', 'root', null)
-    , db        = {}
+var fs        = require('fs')
+  , path      = require('path')
+  , Sequelize = require('sequelize')
+  , lodash    = require('lodash')
+  , sequelize = new Sequelize('sequelize_test', 'root', null)
+  , db        = {}
 
-  fs
-    .readdirSync(__dirname)
-    .filter(function(file) {
-      return (file.indexOf('.') !== 0) && (file !== 'index.js')
-    })
-    .forEach(function(file) {
-      var model = sequelize.import(path.join(__dirname, file))
-      db[model.name] = model
-    })
-
-  Object.keys(db).forEach(function(modelName) {
-    db[modelName].options.associate(db)
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== 'index.js')
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file))
+    db[model.name] = model
   })
 
-  global.db = lodash.extend({ sequelize: sequelize, Sequelize: Sequelize }, db)
-}
+Object.keys(db).forEach(function(modelName) {
+  db[modelName].options.associate(db)
+})
 
-module.exports = global.db
+module.exports = lodash.extend({ sequelize: sequelize, Sequelize: Sequelize }, db)
+
