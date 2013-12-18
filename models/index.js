b6a32672e1f1303e1a6d361262b909499ca8f7e3
sammychl <sammychl@hotmail.com>
2013-12-18 10:14:13 -0800

    Update index.js
    prevents index.js from parsing autosave and/or backup files generated by texteditors. (eg. emacs appends "~" for backup files, wraps "#" around file names for autosave files)

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ var fs        = require('fs')
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== 'index.js')
+    return ((file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) == '.js'))
   })
   .forEach(function(file) {
     var model = sequelize.import(path.join(__dirname, file))
