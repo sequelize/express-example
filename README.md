21d24a4b5c8376d9891e0ac1674051a7a76748b8
Sascha Depold <sascha@depold.com>
2014-10-03 12:45:29 +0200

    Remove old files

--- README.md
+++
@@ -1,4 +0,0 @@
-sequelize-expressjs-example
-===========================
-
-An example that shows a proposal for the usage of Sequelize within an Express.JS application.
--- routes/user.js
+++
@@ -1,7 +0,0 @@
-var db = require('../models')
-
-exports.create = function(req, res) {
-  db.User.create({ username: req.param('username') }).success(function() {
-    res.redirect('/')
-  })
-}
