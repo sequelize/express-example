8448cb11fcdd4d927127ea5c17e4f7e350907519
Jeff Lee <leejeffc@gmail.com>
2015-07-24 13:57:04 -0700

    Fix misnamed reference

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-        message: errorMessage,
+        message: err.message,
         error: (app.get('env') === 'development') ? err : {}
     });
 });
