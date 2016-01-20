3859f4cd4c196fffcb7dffe29ee571105f89dac4
Stéphane Erard <stephane.erard@gmail.com>
2016-01-20 01:10:46 +0100

    Fix require resolve error on windows 10
    After a bare install following instructions, hit this : Error: Cannot find module 'C:\code\testmodels/..configconfig.json'

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
-var config    = require(__dirname + '/../config/config.json')[env];
+var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
