f98ced46d26aa94b36521a797a2aba487a7d6546
bananaapple <bananaappletw@gmail.com>
2017-01-04 23:18:09 +0800

    Add config options (#52)

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
+  var sequelize = new Sequelize(process.env.DATABASE_URL,config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
