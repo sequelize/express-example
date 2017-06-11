0745b9df8d1b22c4c561ca95e4e06590f0e029fd
Peter Valtersson <peter.valtersson@gmail.com>
2017-06-11 21:58:55 +0200

    Fix breaking changes introduced with Sequelize 4.0.0 (#62)
    * Adapt to breaking changes in Sequelize 4.0
    
    classMethods and instancesMethods were removed from options in Sequelize 4.0.0.
    
    * Create task.js
    
    * Create user.js

--- models/task.js
+++ models/task.js
@@ -3,20 +3,18 @@
 module.exports = function(sequelize, DataTypes) {
   var Task = sequelize.define("Task", {
     title: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // Using additional options like CASCADE etc for demonstration
-        // Can also simply do Task.belongsTo(models.User);
-        Task.belongsTo(models.User, {
-          onDelete: "CASCADE",
-          foreignKey: {
-            allowNull: false
-          }
-        });
-      }
-    }
   });
-
+  
+  Task.associate = function(models) {
+    // Using additional options like CASCADE etc for demonstration
+    // Can also simply do Task.belongsTo(models.User);
+    Task.belongsTo(models.User, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  }
+  
   return Task;
 };
--- models/user.js
+++ models/user.js
@@ -3,13 +3,11 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        User.hasMany(models.Task)
-      }
-    }
   });
 
+  User.associate = function(models) {
+    User.hasMany(models.Task);
+  }
+  
   return User;
 };
