7e13c5de37a562c8e1678b3991bd59d61a8153af
John Griffiths <john@uxgent.com>
2017-11-15 18:27:14 -0500

    Switch from Jade to Pug, Jade changed its name.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
