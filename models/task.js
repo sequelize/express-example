6db5e5ce282a99809730866bfe5d44c65063f3eb
Sushant <sushantdhiman@outlook.com>
2017-11-25 14:07:27 +0530

    dont use classMethods

--- models/task.js
+++ models/task.js
@@ -2,17 +2,16 @@
 module.exports = (sequelize, DataTypes) => {
   var Task = sequelize.define('Task', {
     title: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        models.Task.belongsTo(models.User, {
-          onDelete: "CASCADE",
-          foreignKey: {
-            allowNull: false
-          }
-        });
-      }
-    }
   });
+
+  Task.associate = function (models) {
+    models.Task.belongsTo(models.User, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
   return Task;
 };
--- models/user.js
+++ models/user.js
@@ -2,12 +2,11 @@
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     username: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        models.User.hasMany(models.Task);
-      }
-    }
   });
+
+  User.associate = function(models) {
+    models.User.hasMany(models.Task);
+  };
+
   return User;
 };
