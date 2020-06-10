1b5f821d48eaf817b3e9181001d922acfd9134c8
Rakesh Lanjewar <rakesh.sakoli@gmail.com>
2020-06-10 13:21:02 +0530

    Fixed Association and Updated code for v5

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.js')[env];
-var db        = {};
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.js')[env];
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -14,15 +13,13 @@ if (config.use_env_variable) {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname).filter(file => {
+  return (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js');
+}).forEach(function (filename) {
+  sequelize.import(path.join(__dirname, '', filename));
+});
+
+const db = sequelize.models;
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
